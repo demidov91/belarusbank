dbd9d39ac571751765658064846ac2f4987da7eb
Dmitry Demidov <demidov91@msil.ru>
2015-10-14 19:38:35 +0300

    Installation-specific paths are removed.

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
-print('TROLOLO')
 import sys
 import os
-PROJ_PATH = '/home/demidov/belarusbank/'
+PROJ_PATH = os.path.dirname(__file__)
 
 sys.path.insert(0, os.path.join(PROJ_PATH, 'env/lib/python3.4/site-packages/'))
 sys.path.insert(0, PROJ_PATH)
