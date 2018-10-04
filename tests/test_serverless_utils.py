ee744258d7830f53e0b6abf98a5964f108bb76ba
dzmitry <demidov91@mail.ru>
2018-10-05 02:21:20 +0300

    Add little more tests.

--- tests/test_serverless_utils.py
+++ tests/test_serverless_utils.py
@@ -2,6 +2,7 @@ from unittest import mock
 
 from serverless_utils import (
     json_response,
+    no_trailing_slash,
     redirect_response_302,
 )
 
@@ -30,3 +31,28 @@ def test_json_response(patched):
         'body': '{"a": 1}',
     }
     patched.assert_called_once_with(data,  ensure_ascii=False, indent=2)
+
+
+def test_no_trailing_slash__no_slash():
+    wrapped = mock.Mock()
+    wrapper = no_trailing_slash(wrapped)
+
+    event = {'requestContext': {'path': '/some/url'}}
+    context = mock.Mock()
+
+    wrapper(event, context)
+
+    wrapped.assert_called_once_with(event, context)
+
+
+@mock.patch('serverless_utils.redirect_response_302', return_value=42)
+def test_no_trailing_slash__with_slash(patched_redirect):
+    wrapped = mock.Mock()
+    wrapper = no_trailing_slash(wrapped)
+
+    event = {'requestContext': {'path': '/some/url/'}}
+    context = mock.Mock()
+
+    assert wrapper(event, context) == 42
+
+    patched_redirect.assert_called_once_with('/some/url')
