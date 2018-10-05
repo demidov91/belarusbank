64ad9e355947a22dec98eb04d65f22d89e6fa59b
dzmitry <demidov91@mail.ru>
2018-10-05 19:39:35 +0300

    Fix another one merge conflict.

--- mtbank/serverless.py
+++ mtbank/serverless.py
@@ -10,23 +10,23 @@ logger = logging.getLogger(__name__)
 
 
 def _redirect_to_auth(*, next: str, reason: str):
-    return redirect_response_302(f'auth?next={next}&reason={reason}')
+    return redirect_response_302(f'/auth?next={next}&reason={reason}')
 
 
 def overview(event, context):
     if 'headers' not in event or 'Cookie' not in event['headers']:
         logger.info('No cookie')
-        return _redirect_to_auth(next='mtb', reason='no-password')
+        return _redirect_to_auth(next='/mtb', reason='no-password')
 
     cookie = SimpleCookie(event['headers']['Cookie'])
 
     if 'sessionId' not in cookie or 'encryptKey' not in cookie:
         logger.info('No session')
-        return _redirect_to_auth(next='mtb', reason='no-password')
+        return _redirect_to_auth(next='/mtb', reason='no-password')
 
     username, password = get_credentials(cookie['sessionId'].value, cookie['encryptKey'].value)
 
     if not (username and password):
-        return _redirect_to_auth(next='mtb', reason='no-password')
+        return _redirect_to_auth(next='/mtb', reason='no-password')
 
     return json_response(mtbank_utils.overview(username, password))
