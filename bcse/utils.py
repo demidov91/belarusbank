9a714b92377510a089a9e661033c7416585b3ff1
Dzmitry <demidov91@mail.ru>
2020-06-28 15:42:32 +0300

    Ignore https for bcse :(

--- bcse/utils.py
+++ bcse/utils.py
@@ -1,4 +1,5 @@
 import datetime
+import ssl
 from urllib.request import urlopen, Request
 from xml.etree import ElementTree as ET
 
@@ -6,12 +7,15 @@ from serverless_utils import json_response
 
 
 def get_remote_data():
-    url = 'http://www.bcse.by/ru/currencymarket/ratesinformertab?mode=ContinuousDoubleAuction&tstamp={}'.format(
+    url = 'https://www.bcse.by/ru/currencymarket/ratesinformertab?mode=ContinuousDoubleAuction&tstamp={}'.format(
         int(datetime.datetime.now().timestamp() * 1000)
     )
     request = Request(url)
     request.add_header('X-Requested-With', 'XMLHttpRequest')
-    return urlopen(request).read().decode('utf-8').strip()
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    return urlopen(request, timeout=20, context=ctx).read().decode('utf-8').strip()
 
 
 def get_data():
@@ -23,8 +27,11 @@ def get_data():
         currency = tr[0][0].text[:tr[0][0].text.index('/')].strip()
         time = tr[0][1].tail.strip()
         rate = tr[1].text.strip()
-        diff = tr[2][1][0].text.strip()
-        diff = diff[0] + diff[1:].strip()
+        if len(tr[2]) > 1:
+            diff = tr[2][1][0].text.strip()
+            diff = diff[0] + diff[1:].strip()
+        else:
+            diff = tr[2][0].text.strip()
         rates[currency] = '{} ({}) ({})'.format(rate, diff, time)
     return rates
 
